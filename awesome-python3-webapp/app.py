cab4c14d9e80c9e2a8bed141aeb4882f0e296372
yuzhang <443410085@qq.com>
2018-11-17 21:09:59 +0800

    循环参数已被弃用的问题解决

--- awesome-python3-webapp/app.py
+++ awesome-python3-webapp/app.py
@@ -11,13 +11,17 @@ from aiohttp import web
 def index(request):
     return web.Response(body=b'<h1>Awesome</h1>')
 
-async def init(loop):
-    app = web.Application(loop=loop)
+async def init(event_loop):
+    #循环参数已被弃用 
+    app = web.Application()
     app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    app_runner = web.AppRunner(app)
+    srv = await loop.create_server(app_runner.app._make_handler(), '127.0.0.1', 9000)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
+#获取eventloop
 loop = asyncio.get_event_loop()
+#执行coroutine
 loop.run_until_complete(init(loop))
 loop.run_forever()
